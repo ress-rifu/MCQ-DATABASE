39ebcba2d025533411d344f92c508d914dea411c
ress-rifu <22235103225@cse.bubt.edu.bd>
2025-04-11 20:00:25 +0600

    Initialize React project with Vite, Tailwind CSS and essential dependencies

--- fr/vite.config.js
+++ fr/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
@@ -7,7 +8,8 @@ export default defineConfig({
   plugins: [
     react({
       fastRefresh: true,
-    })
+    }),
+    tailwindcss()
   ],
   resolve: {
     alias: {
